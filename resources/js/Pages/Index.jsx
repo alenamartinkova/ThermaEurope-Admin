e95b4805d53926ea1218b036fad2ab148ace8532
Petr Dudek <petr.dudek@moravio.com>
2022-11-21 12:46:05 +0100

    init Inertia

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Head } from '@inertiajs/inertia-react'
+
+export default function Index({ user }) {
+    return (
+        <div>
+            <Head title="Welcome" />
+
+            <h1>Welcome</h1>
+        </div>
+    )
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-import './bootstrap';
+import React from 'react'
+import {render} from 'react-dom'
+import {createInertiaApp} from '@inertiajs/inertia-react'
+
+import '../css/app.css';
+
+createInertiaApp({
+    resolve: async (name) => {
+        return (await import(`./Pages/${name}.jsx`)).default
+    },
+    setup({el, App, props}) {
+        render(React.createElement(App, props), el)
+    },
+})
