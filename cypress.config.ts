eff7be52794440804cd1a9bdb28b476b71e3623d
Petr Dudek <petr.dudek@moravio.com>
2022-12-01 11:56:11 +0100

    cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://admin.thermaeurope.l/',
+    baseUrl: 'http://admin.thermaeurope.l',
     setupNodeEvents (on, config) {
       // implement node event listeners here
     }
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,11 +1,18 @@
 
 Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
+  let auth = {}
+  if (Cypress.env('USERNAME') !== undefined && Cypress.env('PASSWORD') !== undefined) {
+    auth = {
+      auth: {
+        username: Cypress.env('USERNAME'),
+        password: Cypress.env('PASSWORD')
+      }
+    }
+  }
+
   // @ts-expect-error
   return originalFn(url, {
     ...options,
-    auth: {
-      username: Cypress.env('USERNAME'),
-      password: Cypress.env('PASSWORD')
-    }
+    ...auth
   })
 })
