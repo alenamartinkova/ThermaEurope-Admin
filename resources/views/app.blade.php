087876d6cefad803b9a7cc632c0307a7017a2179
Petr Dudek <petr.dudek@moravio.com>
2022-12-21 13:21:00 +0100

    fix https url

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     @routes
+    <script>
+      Ziggy.url = '{{ env('APP_URL') }}'
+    </script>
+
     @vite(['resources/js/app.tsx'])
     @inertiaHead
 </head>
